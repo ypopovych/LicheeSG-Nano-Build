8b12b928990c697eb88cb2032af7cd2a38fd9d86
Yehor Popovych <yehor@popovy.ch>
2026-06-04 15:32:15 +0100

    Adjust memory configurations for RAM expansion
    Update memory size configurations in memmap.py for user space RAM expansion.

--- build-dap.sh
+++ build-dap.sh
@@ -147,7 +147,9 @@ done
 
 # Expand user space RAM from 128MB to 160MB
 if ! grep -q -E '^CONFIG_DDR_64MB_SIZE=y' boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig ; then
-  sed -i s/'ION_SIZE = .* . SIZE_1M'/'ION_SIZE = 75 * SIZE_1M'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
+  sed -i s/'ION_SIZE = .* . SIZE_1M'/'ION_SIZE = 0 * SIZE_1M'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
+  sed -i s/'H26X_BITSTREAM_SIZE = .* . SIZE_1M'/'H26X_BITSTREAM_SIZE = 0 * SIZE_1M'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
+  sed -i s/'ISP_MEM_BASE_SIZE = .* . SIZE_1M'/'ISP_MEM_BASE_SIZE = 0 * SIZE_1M'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
   sed -i s/'BOOTLOGO_SIZE = .* . SIZE_1K'/'BOOTLOGO_SIZE = 5632 * SIZE_1K'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
 fi
 # board config for maixcdk
